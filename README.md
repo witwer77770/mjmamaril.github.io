e4317060b63242e64e97d05721e18392da281738
Mark Jayson <markjayson.mamaril2@gmail.com>
2024-05-03 19:22:44 +0800

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mjmamaril.github.io
---
+++ assets/script/index.js
@@ -0,0 +1,64 @@
+// const firstNum = 15
+// const secondNum = 80
+// const sum = firstNum + secondNum
+
+
+// /*sdfpsldkfs[dfk s[;ldkfs;dlkf;sldk consoler
+// ]]
+// */
+
+// function mySum(x, y) {
+//     return x + y
+// }
+
+// function mySub(x, y) {
+//     return x - y
+// }
+
+// function myProduct(x, y) {
+//     return x * y
+// }
+
+// function myQuo(x, y) {
+//     return x / y
+// }
+
+
+// console.log(mySum(25,12))
+// console.log(mySub(25,12))
+// console.log(myProduct(25,12))
+// console.log(myQuo(25,12))
+
+
+// const myAge = 18
+
+// if (myAge < 18 ) {
+//     console.log("kid")
+// } else if (myAge > 18 ){
+//     console.log("adult")
+// } else {
+//     console.log("tae ka")
+// }
+
+
+//for (let x = 0; x <= 100; x++) {
+//    if (x % 2) {
+//        console.log(x)
+//    } 
+//}
+
+
+
+
+
+
+
+
+
+
+//Dom manipulation
+const body = document.body
+const div = document.createElement("div")
+body.append(div)
+
+div.innerHTML = "hello! kamusta"
